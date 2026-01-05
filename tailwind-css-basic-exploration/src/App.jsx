229bdf250021b6eccfa584047442ae1e5de8e6ee
suchitrasahu996-creator <suchitrasahu996@gmail.com>
2026-01-05 23:46:00 +0530

    adding the answer

---
+++ tailwind-css-basic-exploration/src/App.jsx
@@ -0,0 +1,30 @@
+ export default function App(){
+    return(
+    
+        <div class= "bg-purple-400 h-screen w-full">
+            <div class="bg-purple-600  px-15  text-amber-950 flex justify-between">
+                <h1 class="text-2xl font-extrabold">TAILWIND UI</h1>
+                <nav class="flex gap-10">
+                    <a href="#">HOME</a>
+                    <a href="#">FEATURES</a>
+                    <a href="#">TABLE</a>
+                    <a href="#">CONTACT</a>
+                </nav>
+            </div>
+
+ 
+        <div>
+            <section class="p-8 items-center flex flex-col lg:flex-row gap-5 ">
+            <h1 class="md:text-3xl text-xl lg-text-5xl font-bold bg-clip-text">
+                TAILWIND CSS BASIC EXPLORATION 
+            </h1>
+            <p>TO LEARN UTILITY-FIRST STYLING WITH RESPONSIVE DESIGN</p>
+            <button>GET STARTED</button>
+            </section>
+        </div>
+        <div>
+            <img src=""></img>
+        </div>
+    </div>
+    )
+}
